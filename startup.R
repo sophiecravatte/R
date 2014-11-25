5d60d20a9f97c4f73c3372d73faa42291b678853
NastyZ98 <nathan.agez@gmail.com>
2014-11-25 20:28:27 +1100

    Update and rename Startup.R to startup.R

--- startup.R
+++ startup.R
@@ -1,3 +1,4 @@
+# ======================================V.1.1 by NastyZ98 (Nathan Agez) release b.2.1===========================================
 #Create a new WorkSpace (where .Rhistory files will be save).
 setwd("index")
 #If a WorkSpace is already configured, you can see where.
