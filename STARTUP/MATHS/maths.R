d5d74ac9812d66c6c7c0fa11afc5495bdda066e7
NastyZ98 <nathan.agez@gmail.com>
2014-11-26 22:55:39 +1100

    Update maths.R

--- STARTUP/MATHS/maths.R
+++ STARTUP/MATHS/maths.R
@@ -4,11 +4,11 @@
 #
 #===========================================================================================
 
-#Calcul des anomalies mensuelles réduites
-#pluie <- read.csv(file="../STAGE_IRD/Données/pluies_mensuelles_1961-2014.csv", header=TRUE, sep=";", dec=",")
+#Calcul des anomalies mensuelles rÃ©duites
+#pluie <- read.csv(file="../STAGE_IRD/DonnÃ©es/pluies_mensuelles_1961-2014.csv", header=TRUE, sep=";", dec=",")
 data <- read.csv(file="index", header=TRUE, sep=";", dec=",")
 
-#Calcul d'un indice mensuel selon les stations et les précipitations 
+#Calcul d'un indice mensuel selon les stations et les prÃ©cipitations 
 #============================Code original : =========================================
 #| MEAN=ave(values$VALEUR,factor(values$NOM),factor(values$MOIS),FUN=mean,na.rm=TRUE)|
 #| STD=ave(values$VALEUR,factor(values$NOM),factor(values$MOIS),FUN=sd,na.rm=TRUE)   |
@@ -17,18 +17,18 @@ data <- read.csv(file="index", header=TRUE, sep=";", dec=",")
 #| rm(ANOMALIE,MEAN,STD)                                                             |
 #=====================================================================================
 
-#Création des fonctions mean_na et sd_na
+#CrÃ©ation des fonctions mean_na et sd_na
 mean_na=function(x) {mean(x, na.rm=TRUE)}
 sd_na=function(x) {sd(x,na.rm=TRUE)}
 
-#Calcul dans le cadre des précipitations mensuels (données météo fr ©)
+#Calcul dans le cadre des prÃ©cipitations mensuels (donnÃ©es mÃ©tÃ©o fr Â©)
 MEAN=ave(SELECT_STATION$RRE,factor(SELECT_STATION$NOM),factor(SELECT_STATION$MOIS),FUN=mean_na,na.rm=TRUE)
 STD=ave(SELECT_STATION$RRE,factor(SELECT_STATION$NOM),factor(SELECT_STATION$MOIS),FUN=sd_na,na.rm=TRUE)
 ANOMALIE=(SELECT_STATION$RRE-MEAN)/STD
 PLUIE_ANOMALIE=cbind(SELECT_STATION,ANOMALIE)
 rm(ANOMALIE,MEAN,STD)
 
-#Création du plot
+#CrÃ©ation du plot
 #plot(PLUIE_RSLT$JMA_FINAL[which(PLUIE_RSLT$NOM=="NOUMEA")], PLUIE_RSLT$ANOMALIE[which(PLUIE_RSLT$NOM=="NOUMEA")], type="l", xlab="YEARS", ylab="ANOMALIE")
 plot(data$valeur[which(data$valeur=="valeur")], data$valeur[which(data$valeur=="valeur")], type="l", xlab="x title", ylab="y title")
 
@@ -38,4 +38,4 @@ quantile(data$valeur, na.rm=TRUE)
 
 #tapply
 f_nom = factor(data)
-tapply(data$valeur, f_nom, function, na.rm=)
+tapply(data$valeur, f_nom, function, na.rm=TRUE/FALSE)
