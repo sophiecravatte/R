c983b3de57117f37696f869af1b1f06c3412c4a8
NastyZ98 <nathan.agez@gmail.com>
2014-11-27 22:29:35 +1100

    Create Plot-JFM-NINO3INDEX.R

---
+++ French/Lancement/Plots/JFM/Plot-JFM-NINO3INDEX.R
@@ -0,0 +1,34 @@
+#plot NINO3INDEX J/F/M
+#Convertion
+
+PLUIE_JFM = INDICE_PLUIE[INDICE_PLUIE$month=="01" | INDICE_PLUIE$month=="02" | INDICE_PLUIE$month=="03",]
+f_years = factor(PLUIE_JFM$years)
+class(f_years)
+tapply(PLUIE_JFM$INDICE, f_years, mean, na.rm=TRUE)
+INDICE_ANNUEL_JFM <- tapply(PLUIE_JFM$INDICE, f_years, mean, na.rm=TRUE)
+
+DATE_JFM <- as.Date(PLUIE_MAM$years, "%Y")
+DATE_JFM <- as.Date(paste("15", "02",PLUIE_JFM$years, sep="/"), "%d/%m/%Y")
+
+#plots
+#Plot JFM (Janvier-Fevrier-Mars)
+uni=unique(DATE_JFM)
+plot(unique(DATE_JFM), INDICE_ANNUEL, type="o", main="JAN-FEV-MAR", xlab="ANNEES", ylab="INDICE ANNUEL")
+
+#Points colorés < %25 JFM (Janvier-Fevrier-Mars)
+points(uni[INDICE_ANNUEL < -0.4], INDICE_ANNUEL[INDICE_ANNUEL < -0.4],col="blue",pch=19)
+
+#Filtre
+fil3=rep(1/5,5)
+
+#Création d'une nouvelle variable pour filtre (1/5.5) NINO3INDEX
+nino3_fil=filter(INDICE_NINO$NINO3INDEX/-2,fil3)
+
+#Trace par dessus le plot nino3index
+lines(DATE_NINO, nino3_fil, type="l", col="red")
+
+#Trace une ligne horizontal 
+#Quantile de JAN-FEV-MAR = -0.4019309
+abline(h=-0.4019309, col="green", lwd="3", lty="dotted")
+
+#TEMPLATE NINO3INDEX
